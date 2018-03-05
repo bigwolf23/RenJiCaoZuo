fe8bf020ed0f760c30b02b7ffad98b5e5d4619ec
bigwolf23 <47907541@QQ.com>
2018-03-05 22:39:56 +0800

    Second Version
    Add config file
    Add Call main windows process

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,22 +5,44 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Configuration; 
 
 namespace RenJiCaoZuo
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    /// 
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Application.Current.StartupUri = new Uri(getFirstPageName(), UriKind.Relative);
+        }
+
+        private string getFirstPageName()
+        {
+            
+            string strPageType = ConfigurationManager.AppSettings["FirstPageName"]; 
+
+            if(strPageType == "1")
+            {
+                return "MainWindow.xaml";
+            }
+            else if(strPageType == "2")
+            {
+                return "MainWindow1.xaml";
+            }
+            else if (strPageType == "3")
+            {
+                return "MainWindow2.xaml";
+            }
+            else
+            {
+                return "MainWindow.xaml";
+            }
+        }
+
     }
 
-//     private void Application_Startup(object sender, StartupEventArgs e)       
-//     {           
-// 
-//             Application currApp = Application.Current;
-// 
-//             currApp.StartupUri = new Uri("MainWindow.xaml", UriKind.RelativeOrAbsolute);
-// 
-//     }
 }
